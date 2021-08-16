7fd29da43ca1cda9f782d00d5a09638ec93d1125
Samriddha Chattopadhyay <200020040@iitdh.ac.in>
2021-08-16 14:18:32 +0530

    Solved Q6 LNDNCK

---
+++ 2019/LNDNCK.cpp
@@ -0,0 +1,184 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+#define gfoi(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define gfod(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define foi(i, j) gfoi(i, 0, j, 1)
+#define fod(i, j) gfod(i, 0, j, 1)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define PB push_back
+#define PI 3.1415926535897932384626433832795
+#define endl "\n"
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vii;
+typedef long int l;
+typedef unsigned long int ul;
+typedef long long int ll;
+typedef unsigned long long int  ull;
+typedef pair<int, int> pii;
+typedef map<int, int>::iterator mi;
+
+int minar(int ar[], int n) {
+    int min = *min_element(ar, ar + n);
+    return min;
+}
+
+int maxar(int ar[], int n) {
+    int max = *max_element(ar, ar + n);
+    return max;
+}
+
+void swap (int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition (int arr[], int low, int high)        // function to support quicksort
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)        // to be used only for small arrays and datasets
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void merge(int arr[], int l, int m, int r)          // function to support merge sort
+{
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+    int i = 0;
+    int j = 0;
+    int k = l;
+ 
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[],int l,int r){          // to be used only for larger arrays or datasets
+    if(l>=r){
+        return;
+    }
+    int m =l+ (r-l)/2;
+    mergeSort(arr,l,m);
+    mergeSort(arr,m+1,r);
+    merge(arr,l,m,r);
+}
+
+ll diffsum(vi z, vi p) {
+    ll sum = 0;
+    gfoi(i, 1, z.size(), 1) {
+        int u = z[i];
+        int v = z[i-1];
+        sum += max((p[u] - p[v]), (p[v] - p[u]));
+    }
+    return sum;
+}
+
+ll score (int l, int r, vi b, vi p) {
+    vector<pii> q;
+    vi oddq;
+    vi evenq;
+    gfoi(i, l, r+1, 1) {
+        q.PB({ b[i], i });
+    }
+    sort(all(q));
+    int flag = 0;
+    foi(i, q.size()) {
+        flag = 1-flag;
+        if(flag == 1) {
+            oddq.PB(q[i].second);
+        } else {
+            evenq.PB(q[i].second);
+        }
+    }
+    return (diffsum(oddq, p)+diffsum(evenq, p));
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    vi b(n);
+    vi p(n);
+    foi(i, n) {
+        cin >> b[i] >> p[i];
+    }
+    int query;
+    cin >> query;
+    while(query--) {
+        int l, r;
+        cin >> l >> r;
+        l--; r--;
+        cout << score(l, r, b, p) << endl;
+    }
+    return 0;
+}
